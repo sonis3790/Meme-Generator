b38b95b7b989b64f79e8d90a0483a48da930a49c
sonis3790 <shouryasoni27815@gmail.com>
2024-08-03 13:06:08 +0530

    meme generator

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.btn')
+const api = 'https://meme-api.com/gimme'
+const memeImage = document.getElementById('memeImage')
+const loader = document.getElementById('loader')
+btn.addEventListener('click',generateMeme)
+
+function generateMeme(){
+    memeImage.style.display = 'none'
+    loader.style.display = 'block'
+    fetch(api)
+    .then((response)=>response.json())
+    .then(({url})=>{
+        memeImage.src = url
+        btn.innerText = 'Generate another'
+        loader.style.display = 'none'
+        memeImage.style.display = 'block'
+    })
+}
